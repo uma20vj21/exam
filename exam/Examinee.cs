8c0ad0fd4165024cb8169e83b3ab72625112762b
Yuma Oshima <98353128+uma20vj21@users.noreply.github.com>
2023-04-03 01:06:19 +0900

    コンソール上に受験者情報を表示させる記述の追加。 Examineeクラスをインスタンス化させて使うためにExamineeクラスを追加。

---
+++ exam/Examinee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace _exam
+{
+	public class Examinee
+	{
+        public string Name { get; set; }
+		public int Age { get; set; }
+		public int Score { get; set; }
+		public string Grade { get; set; }
+
+		public void showinfo()
+		{
+			System.Console.WriteLine("---受験者情報---");
+			System.Console.WriteLine("受験者名は" + Name + "です。");
+			System.Console.WriteLine("年齢は"　+ Age + "です。");
+			System.Console.WriteLine("筆記試験の結果は" + Score + "で、実技試験の結果は" + Grade + "になります。");
+		}
+ 	}
+}
+
--- exam/Program.cs
+++ exam/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/**
+ * 
+ *Program.cs 
+ * 
+ * 
+ */
+using System;
 
+namespace _exam {
+
+    public class Program
+    {
+        public static void Main(string[] args) {
+            Examinee examinee = new Examinee();
+            examinee.Name = "田中隆";
+            examinee.Age = 19;
+            examinee.Score = 90;
+            examinee.Grade = "B";
+
+            examinee.showinfo();
+        }
+
+    }
+} 
